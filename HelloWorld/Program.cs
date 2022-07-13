d14a4d898da483c08198160c2d719dcce62d260f
Lucca Vinicius Cardoso Carmelin <81098582+luccaccarmelin@users.noreply.github.com>
2022-07-13 16:44:15 -0300

    csharp code

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 3;
+
+            Console.WriteLine(a / b);
+        }
+    }
+}
